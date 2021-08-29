b5ac7a58e6cb47376b62cdbd47e6781e4b713749
pedrohenriquebrandalisedasilva <web@agenciaade.com.br>
2021-08-29 15:16:38 -0300

    Correção use of NULL

--- pedro_project/pedro_project.ino
+++ pedro_project/pedro_project.ino
@@ -7,14 +7,12 @@
 #include <ESP8266WebServer.h>
 #include <ArduinoJson.h>
 
-
 //PINS DEFINITION
 #define DHTPIN D3
 #define DHTTYPE DHT11
 #define ALERT_LED D1
 #define MOTOR_DIRECTION D2
-#define MOTOR_LED BUILTIN_LED
-
+#define MOTOR_LED LED_BUILTIN
 
 //OBJECT INSTANTIATION
 DHT dht(DHTPIN, DHTTYPE);
@@ -22,7 +20,7 @@ WiFiClient client;
 
 //variables
 const size_t MAX_CONTENT_SIZE = 256;// max size of the HTTP response
-const String host = "192.168.1.13";
+const String host = "192.168.0.12";
 const int httpPort = 8080;
 float lastTemperature = 0.0;
 float lastHumidity = 0.0;
@@ -31,14 +29,12 @@ const float nominalTemperature = 18.0;
 const int measureInterval = 30000;
 bool windowOpen = false;
 
-//functions declaration 
-
+//functions declaration
 void configModeCallback(WiFiManager *myWiFiManager);
 void serverAccess();
 void motorLogic();
 
-
-void setup(){ 
+void setup() {
   pinMode(MOTOR_DIRECTION, OUTPUT);
   pinMode(MOTOR_LED, OUTPUT);
   digitalWrite(MOTOR_DIRECTION, LOW);
@@ -58,16 +54,16 @@ void setup(){
 
 }
 
-void loop(){
+void loop() {
 
-//DynamicJsonDocument doc(MAX_CONTENT_SIZE);
-  
-  
+  //DynamicJsonDocument doc(MAX_CONTENT_SIZE);
+
+
   serverAccess();
   motorLogic();
-  
+
   delay(5000);
-  
+
 }
 
 
@@ -82,12 +78,14 @@ void serverAccess() {
   float temperature, humidity;
   temperature = dht.readTemperature();
   humidity = dht.readHumidity();
-  if(temperature != null){
-    lastTemperature = temperature;
-  }
-  if(humidity != null){
-    lastHumidity = humidity;
-  }
+//  if (temperature != NULL) {
+//    lastTemperature = temperature;
+//  }
+//  if (humidity != NULL) {
+//    lastHumidity = humidity;
+//  }
+  lastTemperature = temperature;
+  lastHumidity = humidity;
 
   const int capacity = JSON_OBJECT_SIZE(2);
   StaticJsonDocument<capacity> doc;
@@ -105,17 +103,17 @@ void serverAccess() {
   }
 
   client.println("POST /measure HTTP/1.1");
-  client.println("Host: "+ host + ":" + httpPort);
+  client.println("Host: " + host + ":" + httpPort);
   client.println("Accept: */*");
   client.println("Content-Type: application/json");
   client.print("Content-Length: ");
   client.println(body.length());
   client.println();
   client.print(body);
-  client.println();   //HTTP request termination empty line 
-  
+  client.println();   //HTTP request termination empty line
 
-  while(!client.available()){}
+
+  while (!client.available()) {}
 
   while (client.available()) {
     String input = client.readStringUntil('\r');
@@ -128,16 +126,16 @@ void serverAccess() {
 
   }
 
-  if (client.connected()) { 
+  if (client.connected()) {
     client.stop();  // DISCONNECT FROM THE SERVER
   }
 }
 
 void motorLogic() {
-  if(lastTemperature >= nominalTemperature){
-    Serial.println("precisa abrir");
-    if(!windowOpen) {
-      Serial.println("abrindo");
+  if (lastTemperature >= nominalTemperature) {
+    Serial.println("Estado: aberto.");
+    if (!windowOpen) {
+      Serial.println("Abrindo.");
       digitalWrite(MOTOR_DIRECTION, HIGH);
       digitalWrite(MOTOR_LED, LOW);
       delay(10000);
@@ -145,9 +143,9 @@ void motorLogic() {
       windowOpen = true;
     }
   } else {
-    Serial.println("precisa fechar");
-    if(windowOpen) {
-      Serial.println("fechando");
+    Serial.println("Estado: fechado.");
+    if (windowOpen) {
+      Serial.println("Fechando.");
       digitalWrite(MOTOR_DIRECTION, LOW);
       digitalWrite(MOTOR_LED, LOW);
       delay(10000);
